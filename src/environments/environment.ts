e3dbf9374728d016af264a6808e3956a392f2d75
ThallesYVasconcelos <thalles.vasconcelos@ccc.ufcg.edu.br>
2026-02-23 01:32:03 -0300

    adição de url de api em cloud

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://delpha-nonaltruistic-bumptiously.ngrok-free.dev',
+  apiUrl: 'https://requirements-assistant-277624386975.us-central1.run.app',
   googleClientId: '277624386975-494evdcf902ttcq2u1j4digsrtobe0ea.apps.googleusercontent.com' 
 };
